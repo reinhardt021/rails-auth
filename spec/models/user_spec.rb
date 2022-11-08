104edeae3fd320e6b3e7643dcaa02f9dcac1ce7a
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-08 12:33:52 -0800

    added dependency to test database setup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "db structure" do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:password_digest).of_type(:string) }
+
+    it { is_expected.to have_db_index(:email) }
+  end
 end
