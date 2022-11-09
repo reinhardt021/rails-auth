96ae69f8fadade693c2da6bb90b8e99b61afb936
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-09 13:49:39 -0800

    added session storage to redirect to register page if not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Signed up!"
     else
       render :new
