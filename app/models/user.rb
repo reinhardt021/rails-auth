3f21c891412f8c1e8367a796aa65e4de7b00fd82
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-09 09:38:17 -0800

    geez that took longer than expected; removed attr_accessor taht was breaking functionality for creating a new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :email, :password, :password_confirmation
-
   has_secure_password
+
   validates :email, presence: true
-  #validates :password, presence: true, confirmation: true, on: :create
+  validates :password, presence: true, confirmation: true, on: :create
 end
