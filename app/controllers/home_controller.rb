9576f84ae4d5d14211f11b5dc08c38989ecf1be1
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-12 00:13:08 -0800

    Have the warden gem workign for register now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if !session[:user_id] then
+    #puts request.env.inspect
+    #puts env['warden']
+    if !request.env['warden'].user then
+    #if !session[:user_id] then
       redirect_to register_url
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      request.env['warden'].set_user @user
+      #session[:user_id] = @user.id
       redirect_to root_path, notice: "Signed up!"
     else
       render :new
