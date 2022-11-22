82192ffa261ac1a82c1c0d5cfa017d1d410ee301
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-23 12:36:46 +1300

    created login route and page annd consolidated CSS for now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,32 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    # TODO: find user by email
+    # if user does NOT exist then error out
+    # if user exists then check if password is good
+    # if authenticated then go to home page
+    # if BAD password then error out
+    #@user = User.new(user_params)
+    #if @user.save
+      #warden.set_user @user
+      #redirect_to root_path, notice: "Signed up!"
+    #else
+      #render :new
+    #end
+  end
+
 
   def destroy
     warden.logout
     redirect_to root_path
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
 
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+
   get '/logout', to: 'sessions#destroy' # TODO: change to proper delete route
 end
