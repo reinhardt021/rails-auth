05691b49285025617fe52d1ac337220096c9939a
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-23 11:32:13 +1300

    have uniqueness check and errors showing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true
+  # TODO: add uniqueness check
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, on: :create
 end
