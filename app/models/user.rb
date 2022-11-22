0eb14f36722dbf304acdd2dbf43f297e774b2b3f
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-23 12:06:55 +1300

    removed unnecessary note

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  # TODO: add uniqueness check
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, on: :create
 end
