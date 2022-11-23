c471f9cb09906f76cfcdd832136ffb075092d76d
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-23 13:14:27 +1300

    did check for login and show error if any

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    @errors = []
     @user = User.new
   end
 
   def create
-    # TODO: find user by email
-    # if user does NOT exist then error out
-    # if user exists then check if password is good
-    # if authenticated then go to home page
-    # if BAD password then error out
-    #@user = User.new(user_params)
-    #if @user.save
-      #warden.set_user @user
-      #redirect_to root_path, notice: "Signed up!"
-    #else
-      #render :new
-    #end
+    @errors = []
+    @user = User.authenticate(params[:email], params[:password])
+    if @user then
+      redirect_to root_path
+    else
+      @errors << "Email or Password is invalid"
+      render :new
+    end
   end
 
 
@@ -24,9 +21,5 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 
-  private
-    def user_params
-      params.require(:user).permit(:email, :password)
-    end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true, on: :create
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
