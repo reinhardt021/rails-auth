0828817ed76f8b7dceabb9f544d9a5e761884ac2
Reinhardt <reinhardt.cgr@gmail.com>
2022-11-23 22:55:44 +1300

    Added some test for new login routes and fixed old register routes

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :routing do
   it { expect(get: '/register').to route_to('users#new') }
-  it { expect(post: '/users').to route_to('users#create') }
+  it { expect(post: '/register').to route_to('users#create') }
 end
 
